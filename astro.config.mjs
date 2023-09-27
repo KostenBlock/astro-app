fc28463850885c14a71137f4b40ac1477871c8c7
KostenBlock <kostenkosta@gmail.com>
2023-09-27 16:34:51 +0300

    first

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
         mode: 'middleware' // or 'standalone'
     }),
     // trailingSlash: "always",
-    output: "hybrid",
+    output: "server",
     build: {
         format: 'file'
     },
